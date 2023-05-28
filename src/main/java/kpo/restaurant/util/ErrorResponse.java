6ceed26fb43e0b18a0bacf48793730137514e53b
Ruslan Galiullin <rusgal000@gmail.com>
2023-05-28 10:59:13 +0300

    open api description of all endpoints

--- src/main/java/kpo/restaurant/util/ErrorResponse.java
+++ src/main/java/kpo/restaurant/util/ErrorResponse.java
@@ -1,8 +1,10 @@
 package kpo.restaurant.util;
+
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.http.HttpStatus;
+
 import java.util.Date;
 
 @Getter
